5eb7f09a8d2265b280a11d0aba70779507b291d0
Rakibul Islam Sarkar <rakibulislam1019@gmail.com>
2025-01-09 19:22:51 +0530

    imgly node module fixed

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  webpack: (config: any, { isServer }: { isServer: boolean }) => {
-    if (!isServer) {
-      // Add ONNX Runtime Web WebAssembly files to be copied to the public directory
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-        crypto: false,
-        'onnxruntime-web/webgpu': false
-
-      };
-    }
-
-    return config;
+  images: {
+      remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'images.unsplash.com',
+          },
+        ],
   },
 
   async headers() {
     return [
       {
-        source: "/:path*",
+        source: "/app/:path*", 
         headers: [
           { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
           { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
