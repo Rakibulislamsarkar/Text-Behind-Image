6dd151f4baa1003c4852fdd794c4db0c83367880
Rakibul Islam Sarkar <rakibulislam1019@gmail.com>
2025-01-09 20:30:31 +0530

    oo

--- next.config.ts
+++ next.config.ts
@@ -6,26 +6,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
-
-  async headers() {
-    return [
-      {
-        source: "/app/:path*",
-        headers: [
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
