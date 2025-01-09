d35398af41d9448fb42db78b3261e3d6304bb332
Rakibul Islam Sarkar <rakibulislam1019@gmail.com>
2025-01-10 01:07:12 +0530

    removed unnecessary things

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,51 +1,87 @@
+// app/app/page.tsx
 "use client";
 
-import React, { useState, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
-import dynamic from "next/dynamic";
+
+import {
+  useSessionContext,
+  useSupabaseClient,
+} from "@supabase/auth-helpers-react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Separator } from "@/components/ui/separator";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { Accordion } from "@/components/ui/accordion";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { ModeToggle } from "@/components/global/mode-toggle";
+import { Profile } from "@/types";
+import TextCustomizer from "@/components/editor/text-customizer";
+
 import { PlusIcon, ReloadIcon } from "@radix-ui/react-icons";
+
 import { removeBackground } from "@imgly/background-removal";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+import RandomColorAd from "@/ads/randomcolor";
+
 import "@/app/fonts.css";
 
-// Dynamic imports for browser-specific components
-const TextCustomizer = dynamic(() => import("@/components/editor/text-customizer"), { ssr: false });
-
-interface TextSet {
-  id: number;
-  text: string;
-  fontFamily: string;
-  top: number;
-  left: number;
-  color: string;
-  fontSize: number;
-  fontWeight: number;
-  opacity: number;
-  shadowColor: string;
-  shadowSize: number;
-  rotation: number;
-}
-
 const Page = () => {
+  const { session } = useSessionContext();
+  const supabaseClient = useSupabaseClient();
+  const [currentUser, setCurrentUser] = useState<Profile>();
+
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   const [isImageSetupDone, setIsImageSetupDone] = useState<boolean>(false);
-  const [removedBgImageUrl, setRemovedBgImageUrl] = useState<string | null>(null);
-  const [textSets, setTextSets] = useState<Array<TextSet>>([]);
+  const [removedBgImageUrl, setRemovedBgImageUrl] = useState<string | null>(
+    null
+  );
+  const [textSets, setTextSets] = useState<Array<any>>([]);
+  const [isPayDialogOpen, setIsPayDialogOpen] = useState<boolean>(false);
   const fileInputRef = useRef<HTMLInputElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const handleUploadImage = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
+  const getCurrentUser = async (userId: string) => {
+    try {
+      const { data: profile, error } = await supabaseClient
+        .from("profiles")
+        .select("*")
+        .eq("id", userId);
+
+      if (error) {
+        throw error;
+      }
+
+      if (profile) {
+        setCurrentUser(profile[0]);
+      }
+    } catch (error) {
+      console.error("Error fetching user profile:", error);
     }
   };
 
-  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleUploadImage = () => {
+    if (currentUser && (currentUser.images_generated < 2 || currentUser.paid)) {
+      if (fileInputRef.current) {
+        fileInputRef.current.click();
+      }
+    } else {
+      alert("You have reached the limit of free generations.");
+      setIsPayDialogOpen(true);
+    }
+  };
+
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     const file = event.target.files?.[0];
     if (file) {
       const imageUrl = URL.createObjectURL(file);
@@ -55,22 +91,22 @@ const Page = () => {
   };
 
   const setupImage = async (imageUrl: string) => {
-    if (typeof window === "undefined") return; // Guard for SSR
     try {
       const imageBlob = await removeBackground(imageUrl);
       const url = URL.createObjectURL(imageBlob);
       setRemovedBgImageUrl(url);
       setIsImageSetupDone(true);
-    } catch (error) {
-      console.error("Error removing background:", error);
-    }
-  };
 
-  const createImageInstance = () => {
-    if (typeof window !== "undefined") {
-      return new window.Image();
+      if (currentUser) {
+        await supabaseClient
+          .from("profiles")
+          .update({ images_generated: currentUser.images_generated + 1 })
+          .eq("id", currentUser.id)
+          .select();
+      }
+    } catch (error) {
+      console.error(error);
     }
-    throw new Error("Cannot create image instance on the server.");
   };
 
   const addNewTextSet = () => {
@@ -100,7 +136,7 @@ const Page = () => {
     );
   };
 
-  const duplicateTextSet = (textSet: TextSet) => {
+  const duplicateTextSet = (textSet: any) => {
     const newId = Math.max(...textSets.map((set) => set.id), 0) + 1;
     setTextSets((prev) => [...prev, { ...textSet, id: newId }]);
   };
@@ -116,7 +152,7 @@ const Page = () => {
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
 
-    const bgImg = createImageInstance();
+    const bgImg = new (window as any).Image();
     bgImg.crossOrigin = "anonymous";
     bgImg.onload = () => {
       canvas.width = bgImg.width;
@@ -125,8 +161,10 @@ const Page = () => {
       ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
 
       textSets.forEach((textSet) => {
-        ctx.save();
-        ctx.font = `${textSet.fontWeight} ${textSet.fontSize * 3}px ${textSet.fontFamily}`;
+        ctx.save(); // Save the current state
+        ctx.font = `${textSet.fontWeight} ${textSet.fontSize * 3}px ${
+          textSet.fontFamily
+        }`;
         ctx.fillStyle = textSet.color;
         ctx.globalAlpha = textSet.opacity;
         ctx.textAlign = "center";
@@ -135,14 +173,15 @@ const Page = () => {
         const x = (canvas.width * (textSet.left + 50)) / 100;
         const y = (canvas.height * (50 - textSet.top)) / 100;
 
+        // Move the context to the text position and rotate
         ctx.translate(x, y);
-        ctx.rotate((textSet.rotation * Math.PI) / 180);
-        ctx.fillText(textSet.text, 0, 0);
-        ctx.restore();
+        ctx.rotate((textSet.rotation * Math.PI) / 180); // Convert degrees to radians
+        ctx.fillText(textSet.text, 0, 0); // Draw text at the origin (0, 0)
+        ctx.restore(); // Restore the original state
       });
 
       if (removedBgImageUrl) {
-        const removedBgImg = createImageInstance();
+        const removedBgImg = new (window as any).Image();
         removedBgImg.crossOrigin = "anonymous";
         removedBgImg.onload = () => {
           ctx.drawImage(removedBgImg, 0, 0, canvas.width, canvas.height);
@@ -165,132 +204,127 @@ const Page = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen">
-      <header className="flex flex-row items-center justify-between p-5 px-10">
-        <div className="flex items-center gap-2">
-          <Image src="/opal-logo.svg" width={30} height={30} alt="Opal Logo" />
-          <span className="text-2xl sm:text-3xl font-bold tracking-tight bg-gradient-to-b from-white to-zinc-500 inline-block text-transparent bg-clip-text">
-            Opal
-          </span>
-        </div>
-        <div className="flex gap-4 items-center">
-          <input
-            type="file"
-            ref={fileInputRef}
-            style={{ display: "none" }}
-            onChange={handleFileChange}
-            accept=".jpg, .jpeg, .png"
-          />
-          <div className="flex items-center gap-5">
-            <div className="flex gap-2">
-              <Button onClick={handleUploadImage}>Upload image</Button>
-              {selectedImage && (
-                <Button onClick={saveCompositeImage} className="hidden md:flex">
+    <>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1609710199882100"
+        crossOrigin="anonymous"
+      ></script>
+
+      <div className="flex flex-col h-screen">
+        <header className="flex flex-row items-center justify-between p-5 px-10">
+          <h2 className="text-4xl md:text-2xl font-semibold tracking-tight">
+            <span className="block md:hidden">Opal</span>
+            <span className="hidden md:block">Opal</span>
+          </h2>
+          <div className="flex gap-4 items-center">
+            <input
+              type="file"
+              ref={fileInputRef}
+              style={{ display: "none" }}
+              onChange={handleFileChange}
+              accept=".jpg, .jpeg, .png"
+            />
+            <div className="flex items-center gap-5">
+              <div className="flex gap-2">
+                <Button onClick={handleUploadImage}>Upload image</Button>
+                {selectedImage && (
+                  <Button
+                    onClick={saveCompositeImage}
+                    className="hidden md:flex"
+                  >
+                    Save image
+                  </Button>
+                )}
+              </div>
+            </div>
+            <ModeToggle />
+          </div>
+        </header>
+        <Separator />
+        {selectedImage ? (
+          <div className="flex flex-col md:flex-row items-start justify-start gap-10 w-full h-screen px-10 mt-2">
+            <div className="flex flex-col items-start justify-start w-full md:w-1/2 gap-4">
+              <canvas ref={canvasRef} style={{ display: "none" }} />
+              <div className="flex items-center gap-2">
+                <Button onClick={saveCompositeImage} className="md:hidden">
                   Save image
                 </Button>
-              )}
-            </div>
-          </div>
-          <ModeToggle />
-        </div>
-      </header>
-      <Separator />
-      {selectedImage ? (
-        <div className="flex flex-col md:flex-row items-start justify-start gap-10 w-full h-screen px-10 mt-2">
-          <div className="flex flex-col items-start justify-start w-full md:w-1/2 gap-4">
-            <canvas ref={canvasRef} style={{ display: "none" }} />
-            <div className="flex items-center gap-2">
-              <Button onClick={saveCompositeImage} className="md:hidden">
-                Save image
-              </Button>
-            </div>
-            <div className="min-h-[400px] w-[80%] p-4 border border-border rounded-lg relative overflow-hidden">
-              {isImageSetupDone ? (
-                <Image
-                  src={selectedImage}
-                  alt="Uploaded"
-                  layout="fill"
-                  objectFit="contain"
-                  objectPosition="center"
-                />
-              ) : (
-                <span className="flex items-center justify-center w-full gap-2">
-                  <ReloadIcon className="animate-spin" /> Loading, please wait
-                </span>
-              )}
-              {isImageSetupDone &&
-                textSets.map((textSet) => (
-                  <div
-                    key={textSet.id}
-                    style={{
-                      position: "absolute",
-                      top: `${50 - textSet.top}%`,
-                      left: `${textSet.left + 50}%`,
-                      transform: `translate(-50%, -50%) rotate(${textSet.rotation}deg)`,
-                      color: textSet.color,
-                      textAlign: "center",
-                      fontSize: `${textSet.fontSize}px`,
-                      fontWeight: textSet.fontWeight,
-                      fontFamily: textSet.fontFamily,
-                      opacity: textSet.opacity,
-                    }}
-                  >
-                    {textSet.text}
-                  </div>
-                ))}
-              {removedBgImageUrl && (
-                <Image
-                  src={removedBgImageUrl}
-                  alt="Removed bg"
-                  layout="fill"
-                  objectFit="contain"
-                  objectPosition="center"
-                  className="absolute top-0 left-0 w-full h-full"
-                />
-              )}
-            </div>
-          </div>
-          <div className="flex flex-col w-full md:w-1/2">
-            <Button variant={"secondary"} onClick={addNewTextSet}>
-              <PlusIcon className="mr-2" /> Add New Text Set
-            </Button>
-            <ScrollArea className="h-[calc(100vh-10rem)] p-2">
-              <Accordion type="single" collapsible className="w-full mt-2">
-                {textSets.map((textSet) => (
-                  <TextCustomizer
-                    key={textSet.id}
-                    textSet={textSet}
-                    handleAttributeChange={handleAttributeChange}
-                    removeTextSet={removeTextSet}
-                    duplicateTextSet={duplicateTextSet}
+              </div>
+              <div className="min-h-[400px] w-[80%] p-4 border border-border rounded-lg relative overflow-hidden">
+                {isImageSetupDone ? (
+                  <Image
+                    src={selectedImage}
+                    alt="Uploaded"
+                    layout="fill"
+                    objectFit="contain"
+                    objectPosition="center"
                   />
-                ))}
-              </Accordion>
-            </ScrollArea>
+                ) : (
+                  <span className="flex items-center w-full gap-2">
+                    <ReloadIcon className="animate-spin" /> Loading, please wait
+                  </span>
+                )}
+                {isImageSetupDone &&
+                  textSets.map((textSet) => (
+                    <div
+                      key={textSet.id}
+                      style={{
+                        position: "absolute",
+                        top: `${50 - textSet.top}%`,
+                        left: `${textSet.left + 50}%`,
+                        transform: `translate(-50%, -50%) rotate(${textSet.rotation}deg)`,
+                        color: textSet.color,
+                        textAlign: "center",
+                        fontSize: `${textSet.fontSize}px`,
+                        fontWeight: textSet.fontWeight,
+                        fontFamily: textSet.fontFamily,
+                        opacity: textSet.opacity,
+                      }}
+                    >
+                      {textSet.text}
+                    </div>
+                  ))}
+                {removedBgImageUrl && (
+                  <Image
+                    src={removedBgImageUrl}
+                    alt="Removed bg"
+                    layout="fill"
+                    objectFit="contain"
+                    objectPosition="center"
+                    className="absolute top-0 left-0 w-full h-full"
+                  />
+                )}
+              </div>
+            </div>
+            <div className="flex flex-col w-full md:w-1/2">
+              <Button variant={"secondary"} onClick={addNewTextSet}>
+                <PlusIcon className="mr-2" /> Add New Text Set
+              </Button>
+              <ScrollArea className="h-[calc(100vh-10rem)] p-2">
+                <Accordion type="single" collapsible className="w-full mt-2">
+                  {textSets.map((textSet) => (
+                    <TextCustomizer
+                      key={textSet.id}
+                      textSet={textSet}
+                      handleAttributeChange={handleAttributeChange}
+                      removeTextSet={removeTextSet}
+                      duplicateTextSet={duplicateTextSet}
+                    />
+                  ))}
+                </Accordion>
+              </ScrollArea>
+            </div>
           </div>
-        </div>
-      ) : (
-        <div className="flex items-center justify-center flex-grow p-8">
-        <Card className="w-full max-w-md">
-          <CardHeader>
-            <CardTitle className="text-2xl font-bold text-center">Welcome to Opal</CardTitle>
-            <CardDescription className="text-center">
-              Get started by uploading an image to begin editing
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="flex flex-col items-center space-y-4">
-            <Image
-              src="/bear.png"
-              width={200}
-              height={200}
-              alt="Upload Illustration"
-            />
-            
-          </CardContent>
-        </Card>
+        ) : (
+          <div className="flex items-center justify-center min-h-screen w-full">
+            <h1 className="text-4xl lg:text-5xl xl:text-5xl tracking-tight font-medium p-5 bg-gradient-to-b from-white to-zinc-500 inline-block text-transparent bg-clip-text">
+              Welcome, get started by uploading an image!
+            </h1>
+          </div>
+        )}
       </div>
-      )}
-    </div>
+    </>
   );
 };
 
